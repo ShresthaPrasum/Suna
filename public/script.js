f8a77477f4bfda7bdb07fdaa91baa663896eb1b0
chill-guy77 <kiragyawali8@gmail.com>
2026-09-25 16:46:59 +0545

    made the song_data array go from the file to server then to the frontend

---
+++ public/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', (e)=>{
+    fetch('/get', {
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+            msg: 'HELOOO',
+            msgg: "HELOOO"
+        })
+        })
+        .then(response=>response.json())
+        .then(result=>{
+            if(result.success){
+                const song_data = result.song_data;
+                const paths = result.paths;
+                const covers = result.covers;
+                console.log(song_data);
+                console.log(paths);
+            }
+        })
+    .catch(error=> console.error(error));
+})
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const port = 5000;
 const app = express();
-const fs = require('fs');
+const fs = require('fs');;
 const path = require('path');
-
-const data = fs.readFileSync('./song_data.json');
-const parsed = JSON.parse(data);
-console.log(parsed);
+const pathhs = [];
+const coverss = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+const data = fs.readFileSync('./song_data.json');
+const song_data = JSON.parse(data);
+console.log(song_data)
+for(let i=0; i<song_data.length; i++){
+    const pathh = song_data[i].pathh;
+    const cover = song_data[i].cover
+    pathhs.push(pathh);
+    coverss.push(cover);
+}
+
+app.post('/get',(req,res)=>{
+    const msg = req.body.msg;
+    res.json({
+        song_data: song_data,
+        paths: pathhs,
+        covers: coverss,
+        success: true
+    })
+    console.log(msg);
+})
 
 app.listen(port, ()=>{
     console.log(`Server running on http://localhost:${port}`);
