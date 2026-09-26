ae53d3442ce111f9af0d04abdecccd6de3e2565f
prasum shrestha <prasumshrestha8877@gmail.com>
2026-09-26 12:56:10 +0545

    Feat: Added new music artists and their songs, enhanced scrollbar styling for singers section

--- public/script.js
+++ public/script.js
@@ -19,6 +19,72 @@ document.addEventListener('DOMContentLoaded', (e)=>{
         })
     .catch(error=> console.error(error));
 
+    const musics = {
+        arctic_monkey: [
+            {'doiwannaknow': 'Do I wanna Know'},
+            {'ru_mine': 'R U Mine'},
+            {'505': '505'},
+            {'kneesocks': 'Knee Socks'},
+            {'rumine':'R U Mine'},
+            {'no1partyanthem': 'No. 1 Party Anthem'},
+            {'arabella': 'Arabella'},
+            {'snapoutofit': 'snapoutofit'}
+        ]
+        ,
+        lana_del_rey:[
+            {'summertimesadness': 'Summertime Sadness'},
+            {'youngandbeautiful': 'Young and Beautiful'},
+            {'borntodie': 'Born To DIe'},
+            {'bluejeans':'Blue Jeans'},
+            {'westcoast': 'West Coast'},
+            {'lustforlife': 'Lust For Life'},
+            {'sayyestoheaven': 'Say Yes To Heavem'}
+        ],
+        kendrick_lamar:[
+            {'humble': 'Humble'},
+            {'allthestars': 'All The Stars'},
+            {'notlikeus': 'Not Like Us'},
+            {'moneytree':'Money Tree'},
+            {'loyalty': 'Loyalty'},
+            {'peekaboo': 'Peekaboo'}
+        ],
+        yabesh_thapa:[
+            {'kasari': 'Kasari'},
+            {'aakhale': 'Aakhale'},
+            {'aemutu': 'Ae Mutu'},
+            {'alaptra':'Alaptra'},
+            {'fewataal': 'Fewataal'},
+            {'firfirey': 'Firfirey'}
+        ],
+
+        nepali:[
+            {'kyomayaho': 'K yo Maya Ho'},
+            {'aakhakobato': 'Aakhako Bato'},
+            {'suna': 'Suna'},
+            {'maskimaski': 'Maski Maski'},
+            {'nihita': 'Nihita'},
+            {'vananamatrw': 'Vanana Matrw'}
+        ],
+
+        imagine_dragons:[
+            {'believer': 'Believer'},
+            {'demons': 'Demons'},
+            {'radioactive': 'Radioactive'},
+            {'thunder':'Thunder'},
+            {'whateverittakes': 'Whatever It Takes'},
+            {'bones': 'Bones'}
+        ],
+
+        eminem:[
+            {'loseyourself': 'Lose Yourself'},
+            {'stan': 'Stan'},
+            {'notafraid': 'Not Afraid'},
+            {'therealSlimShady':'The Real Slim Shady'},
+            {'withoutme': 'Without Me'},
+            {'rapgod': 'Rap God'}
+        ]
+    }
+
     let artists = document.querySelectorAll('.singer');
 
     artists.forEach((artist)=>{
